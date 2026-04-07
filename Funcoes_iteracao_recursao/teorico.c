bd46766ced6f5d35772ee67986d996f846b227c6
Velho008 <gabrielvelho08@gmail.com>
2026-04-07 19:29:38 -0300

    nada

--- Funcoes_iteracao_recursao/teorico.c
+++ Funcoes_iteracao_recursao/teorico.c
@@ -1,21 +1,8 @@
 #include <stdio.h>
 
-int main() {
-    char nome[50];
-    int idade;
-
-    printf("=== TESTE C / VS CODE ===\n");
-
-    printf("Digite seu nome: ");
-    scanf("%49s", nome);
-
-    printf("Digite sua idade: ");
-    scanf("%d", &idade);
-
-    printf("\nOla %s!\n", nome);
-    printf("Voce tem %d anos.\n", idade);
-
-    printf("\nSe chegou ate aqui, tudo esta funcionando!\n");
+int main() 
+{
+        
 
     return 0;
 }
