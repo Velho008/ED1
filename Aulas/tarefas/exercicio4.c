dd3b3c410f2a4ea7f78b65ea0e5e784078b10e52
Velho008 <gabrielvelho08@gmail.com>
2026-04-21 12:29:06 -0300

    feita questao 4

--- Aulas/tarefas/exercicio4.c
+++ Aulas/tarefas/exercicio4.c
@@ -1,14 +1,42 @@
+#include<stdio.h>
+#include<string.h>
 /*
 Definindo uma Struct
 
-Objetivo:Definir uma structpara armazenar as informações de um livro.
+Objetivo:Definir uma struct para armazenar as informações de um livro.
 Descrição:
 A struct deverá conter os seguintes campos:
-    1.Título:Stringde 100 caracteres
-    2.Autor:Stringde 50 caracteres
+    1.Título:String de 100 caracteres
+    2.Autor:String de 50 caracteres
     3.Ano de publicação:Inteiro
     4.Número de páginas:Inteiro
 
-Tarefa:Escreva o código para a definição da structe crie duas variáveis dessa structpara armazenar informações de dois livros diferentes.
+Tarefa:Escreva o código para a definição da struct e crie duas variáveis dessa struct para armazenar informações de dois livros diferentes.
 Mostre como você inicializaria os dados para essas duas variáveis.
 */
+
+typedef struct 
+{
+    char titulo[100];
+    char autor[50];
+    int anoPublicacao;
+    int numPaginas;
+}Livro;
+
+Livro setLivro(char titulo[100], char autor[50], int anoPublicacao, int numPaginas)
+{
+    Livro l;
+    strcpy(l.titulo, titulo);
+    strcpy(l.autor, autor);
+    l.anoPublicacao = anoPublicacao;
+    l.numPaginas = numPaginas;
+    return l;
+}
+
+int main()
+{
+    Livro livro1 = setLivro("um livro ai", "eu mesmo", 1892, 67);
+    Livro livro2 = setLivro("um autor legal :)", "aqui um autor", 1666, 677);
+
+    return 0;
+}
