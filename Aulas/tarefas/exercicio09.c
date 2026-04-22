b88a4bfa2bb8e4fe7c4f3235df2fb3abf58f2846
Velho008 <gabrielvelho08@gmail.com>
2026-04-22 14:50:28 -0300

    feito exercicio 9

--- Aulas/tarefas/exercicio09.c
+++ Aulas/tarefas/exercicio09.c
@@ -1,3 +1,16 @@
+#include <stdio.h>
 /*
 Criar um programa que recebe o endereço de um número e imprime o valor desse número.
 */
+
+void imprimeValor(int *ponteiro)
+{
+    printf("%d", *ponteiro);
+}
+
+int main()
+{
+    int numero = 8;
+    imprimeValor(&numero);
+    return 0;
+}
