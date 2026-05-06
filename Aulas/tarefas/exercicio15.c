a11eb58f1d644fbfa1f119198934e3bdaf842247
Velho008 <gabrielvelho08@gmail.com>
2026-05-06 15:06:08 -0300

    adicionados enunciados dos exercicios do slide

---
+++ Aulas/tarefas/exercicio15.c
@@ -0,0 +1,6 @@
+/*  Input record for employ
+    –Name
+    –Age
+    –Basic salary
+    Input record as much as user required
+    Write record in file employee.dat   */
---
+++ Aulas/tarefas/exercicio16.c
@@ -0,0 +1,4 @@
+/*  Write a program to display the content of file that have been created in the previous example program
+    –Name
+    –Age
+    –Basic salary   */
