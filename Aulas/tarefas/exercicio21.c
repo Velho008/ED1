a037f2eb9c6a2aa89529994166e38c72bef3c230
Velho008 <gabrielvelho08@gmail.com>
2026-05-10 16:59:43 -0300

    exe21 feito

--- Aulas/tarefas/exercicio21.c
+++ Aulas/tarefas/exercicio21.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 /*  Exercício 5 — Vetor com ponteiros
     Dado um vetor:
     int v[5] = {10, 20, 30, 40, 50};
@@ -6,3 +7,23 @@
         Imprima os valores
         Some todos os elementos usando apenas aritmética de ponteiros
     Dica: use *(p + i) ou incremento de ponteiro    */
+
+int main()
+{
+    int v[5] = {10, 20, 30, 40, 50};
+    int soma = 0;
+    int *ptr, *ptr2;
+    ptr2 = &soma;
+    ptr = v;
+
+    for(int i=0; i < ((sizeof(v))/sizeof(v[0])); i++)
+    {
+        printf("%d\n", *ptr);
+        *ptr2 += *ptr; //vai fazendo a soma
+        ptr=ptr+1; //passa o ponteiro pro proximo indice com aritmetica de ponteiro
+    }
+
+    printf("soma: %d", *ptr2);
+
+    return 0;
+}
