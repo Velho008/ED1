6a8f86c63a1c04cc9bfbded3a80efee10c837d67
Velho008 <gabrielvelho08@gmail.com>
2026-05-10 16:24:29 -0300

    exe 18 feitp

--- Aulas/tarefas/exercicio18.c
+++ Aulas/tarefas/exercicio18.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 /*  Exercício 2 — Ponteiros cruzados
     Dado:
         int A = 10, B = 20;
@@ -10,3 +11,25 @@
         Imprima novamente
         
     Verifique: O que mudou? O que permaneceu igual?    */
+
+int main()
+{
+    int A = 10, B = 20;
+    int *p1, *p2, *temp;
+    p1 = &A;
+    p2 = &B;
+
+    printf("Antes:\n");
+    printf("p1 aponta para: %d\n", *p1);
+    printf("p2 aponta para: %d\n", *p2);
+
+    temp = p1;
+    p1 = p2;
+    p2 = temp;
+
+    printf("\nDepois:\n");
+    printf("p1 aponta para: %d\n", *p1);
+    printf("p2 aponta para: %d\n", *p2);
+
+    return 0;
+}
