86bbf0315b3603d69df2d331437321427031c4f2
Velho008 <gabrielvelho08@gmail.com>
2026-05-11 11:30:48 -0300

    feito exercicio 25

--- Aulas/tarefas/exercicio25.c
+++ Aulas/tarefas/exercicio25.c
@@ -8,11 +8,37 @@
     Use dois ponteiros (início e fim)
     
     Exemplo: [1, 2, 3, 4] → [4, 3, 2, 1]    */
+//MEU MELHOR CODIGO DE ED1 ATÉ ENTÃO, tive que pensar mto pra fazer
 
 void inverte(int *v, int tamanho)
+{
+    //não trata de overflow nem vetor nulo pq o exercicio não pede
+
+    int *inicio, *fim;
+    inicio = &v[0];
+    fim = &v[tamanho - 1];
+
+    for (int i = 0; i < tamanho/2; i++)
     {
-        for (int i = 0; i < tamanho; i++)
-        {
-            
-        }
+        *inicio+=*fim;
+        *fim = *inicio - *fim;
+        *inicio-=*fim;
+
+        inicio++;
+        fim--;
+    }  
+}
+
+int main()
+{
+    int v[5] = {1, 2, 3, 4, 5};
+
+    inverte(v, 5);
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d ", v[i]);
     }
+
+    return 0;
+}
