48a140b2a587a24a71b2c7356ba201aafcc0980c
Velho008 <gabrielvelho08@gmail.com>
2026-05-11 00:14:41 -0300

    feito exe24

--- Aulas/tarefas/exercicio24.c
+++ Aulas/tarefas/exercicio24.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 /*  Exercício 8 — Contagem de caracteres
     Dada uma string:
         char nome[] = "programacao";
@@ -7,3 +8,28 @@
         
     Retorne quantas vezes c aparece
     Use apenas ponteiros    */
+
+    int conta(char *str, char c)
+    {
+        int repeticoes = 0;
+
+        while (*str != '\0') //enquanto o caractere atual NÃO for o fim da string, pois toda string termina com '\0'
+        {
+            if (*str == c)
+            {
+                repeticoes++;
+            }
+            str++; //aritmetica de ponteiro, é pra ir andando na lista
+        }
+
+        return repeticoes;
+    }
+
+int main()
+{
+    char nome[] = "programacao";
+
+    printf("%d", conta(nome, 'a'));
+
+    return 0;
+}
