58914e27f9dfe4c0d8fa9a7c1413f1bb0db997cb
Velho008 <gabrielvelho08@gmail.com>
2026-05-11 00:17:51 -0300

    exe25 ainda nao feito, nao sei o que fazer agora

--- Aulas/tarefas/exercicio25.c
+++ Aulas/tarefas/exercicio25.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 /*  Exercício 9 — Inversão de vetor (in-place)
     Crie uma função:
         void inverte(int *v, int tamanho);
@@ -7,3 +8,11 @@
     Use dois ponteiros (início e fim)
     
     Exemplo: [1, 2, 3, 4] → [4, 3, 2, 1]    */
+
+void inverte(int *v, int tamanho)
+    {
+        for (int i = 0; i < tamanho; i++)
+        {
+            
+        }
+    }
