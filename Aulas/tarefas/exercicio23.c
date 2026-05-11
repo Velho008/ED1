59a92865fa9f4697d94076ba6fe2541a88caa30c
Velho008 <gabrielvelho08@gmail.com>
2026-05-11 00:08:50 -0300

    exe 23 feito

--- Aulas/tarefas/exercicio23.c
+++ Aulas/tarefas/exercicio23.c
@@ -1,6 +1,29 @@
+#include <stdio.h>
 /*  Exercício 7 — Maior elemento do vetor
     Implemente:
         int maior(int *v, int tamanho);
 
     Use apenas ponteiros (sem v[i])
     Retorne o maior valor    */
+
+int maior(int *v, int tamanho)
+{
+    int maior = *v; //nao usei 0 pq e se o vetor for somente negativo?
+    for (int i = 0; i < tamanho; i++)
+    {
+        if (*v > maior)
+        {
+            maior = *v;
+        }
+        v = (v+1);
+    }
+    return maior;
+}
+
+int main()
+{
+    int v[5] = {10, 20, 30, 40, 50};
+    printf("Maior: %d", maior(v, 5));
+
+    return 0;
+}
