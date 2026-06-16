6a69a477678904e95ebf6a6cf1ea3cdf38485142
Velho008 <gabrielvelho08@gmail.com>
2026-06-16 12:40:12 -0300

    adiciona anotações da ultima aula

---
+++ Aulas/pilhasEFilas.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+uma pilha é uma estrutura de dados do tipo LIFO (Last In, First Out)
+o último elemento inserido é o primeiro a ser removido
+
+uma pilha não permite acesso aleatorio aos seus elementos
+a inclusão de um novo elemento é feita por meio do empilhamento
+a exclusão de um elemento é feita por meio do desempilhamento
+
+
+
+uma fila é uma estrutura de dados do tipo FIFO (First In, First Out)
+o primeiro elemento inserido é o primeiro a ser removido
+
+uma fila não permite acesso aleatorio aos seus elementos
+a inclusão de um novo elemento é feita por meio do enfileiramento
+a exclusão de um elemento é feita por meio do desenfileiramento
+*/
+
+int main()
+{
+
+    return 0;
+}
